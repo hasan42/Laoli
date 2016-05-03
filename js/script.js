24500fbca5c027815397643fdf47fd9b2504c0eb
hasan42 <hasan83842@gmail.com>
2016-05-03 14:26:02 +0700

    Adaptiv
    Dobavil adaptiv

--- js/script.js
+++ js/script.js
@@ -147,7 +147,27 @@ $(document).ready(function(){
 	$('.sertificat-slider').slick({
 		infinite: true,
 		slidesToShow: 4,
-		slidesToScroll: 1
+		slidesToScroll: 1,
+		responsive: [
+			{
+			  breakpoint: 1000,
+			  settings: {
+				slidesToShow: 3
+			  }
+			},
+			{
+			  breakpoint: 800,
+			  settings: {
+				slidesToShow: 2
+			  }
+			},
+			{
+			  breakpoint: 500,
+			  settings: {
+				slidesToShow: 1
+			  }
+			}
+		  ]
 	});
 	
 	//gallery
